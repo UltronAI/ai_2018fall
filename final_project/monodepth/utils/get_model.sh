35aa1c32a7ce9cd3cc758f78817c7ddda6272ff2
Feng Gao <gaof.student@gmail.com>
2018-12-26 23:03:42 -0500

    update get_model in monodepth

--- final_project/monodepth/utils/get_model.sh
+++ final_project/monodepth/utils/get_model.sh
@@ -2,12 +2,14 @@ model_name=$1
 output_location=$2
 
 filename=$model_name.zip
+mkdir -p $output_location/$model_name
+save_path=$output_location/$model_name
 
 url=http://visual.cs.ucl.ac.uk/pubs/monoDepth/models/$filename
 
-output_file=$output_location/$filename
+output_file=$save_path/$filename
 
 echo "Downloading $model_name"
 wget -nc $url -O $output_file
-unzip $output_file -d $output_location
+unzip $output_file -d $save_path
 rm $output_file
